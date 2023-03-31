f7de5e104721b5cd388f5a22aa0aab56a3455212
Springcomp <springcomp@users.noreply.github.com>
2023-03-31 17:15:23 +0200

    atoms and basic quantifiers

--- src/Checker.ts
+++ src/Checker.ts
@@ -1,8 +1,219 @@
+/*
+IRegexpChecker is pushdown automaton that very quickly determines if a
+regular expression using a specified interoperable subset¹ is
+syntactically correct.
+
+¹ - https://www.ietf.org/archive/id/draft-bormann-jsonpath-iregexp-02.html
+
+*/
+
+/* This code is inspired from JSON_checker.c whose license is show hereafter. */
+
+/* 2007-08-24 */
+
+/*
+Copyright (c) 2005 JSON.org
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+The Software shall be used for Good, not Evil.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+const __ = -1; // the universal error code
+
+export type State = number;
+export type Class = number;
+
+const C_NC: Class = 0; // normal char
+const C_QU: Class = 1; // quantifier
+
+const ascii_class: Class[] = [
+    /*
+        This array maps the 128 ASCII characters into character classes.
+        The remaining Unicode characters are normal characters C_NC.
+    */
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+    __,     __,     C_QU,   C_QU,   __,     __,     __,     __,
+    __,     __,     __,     __,     __,     __,     __,     __, 
+    __,     __,     __,     __,     __,     __,     __,     C_QU, 
+
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+    C_NC,   C_NC,   C_NC,   __,     __,     __,     C_NC,   C_NC,
+
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+    C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,   C_NC,
+    C_NC,   C_NC,   C_NC,   __,     __,     __,     C_NC,   C_NC,
+];
+
+const GO: State = 0; //  start
+const OK: State = 1; //  ok
+
+const state_transition_table: State[][] = [
+  /*
+  The state transition table takes the current state and the current symbol,
+  and returns either a new state or an action. An action is represented as a
+  negative number. A regular expression is accepted if at the end of the text
+  the state is OK and if the mode is DONE.
+
+                  NC  QU */
+  /* start GO */ [-2, __],
+  /* ok    OK */ [-2, -3],
+];
+
+// these modes can be pushed on the stack
+export enum Mode {
+  DONE,
+  ATOM,
+}
+
 class IRegexpChecker {
-  public check(expression: string): boolean {
-    return expression !== null;
+
+  constructor() {
+
+    // starts the checking process by constructing a new
+    // IRegexpChecker object.
+
+    // To continue the process, call() check for each character
+    // in the regular expression text, and then call finalCheck()
+    // to obtain the final result.
+
+    this.state = GO;
+    this.offset = 0;
+    this.stack = []
+    this.quantifiable = false;
+    this.push(Mode.DONE);
+  }
+
+  private state: State;
+  private offset: number;
+  private stack: Mode[];
+  private quantifiable: boolean;
+
+  public check(ch: number):void {
+
+    // After constructing an IRegexpChecker object, call this
+    // function for each character (or partial character) in 
+    // the regular expression.
+    // It returns if things are looking ok so far.
+    // If it rejects the text, it throw an error.
+
+    //console.log(`${this.offset} ch: ${ch}, state: ${this.state}`);
+
+    let nextClass: Class = 0;
+    let nextState: State = 0;
+
+    // determine the character's class
+
+    if (ch < 0) {
+      this.onError();
+    }
+    if (ch >= 128) {
+      nextClass = C_NC;
+    } else {
+      nextClass = ascii_class[ch];
+      //console.log(nextClass);
+      if (nextClass <= __){
+        this.onError();
+      }
+    }
+
+    // get the next state from the state transition table
+
+    nextState = state_transition_table[this.state][nextClass];
+
+    //console.log(`${this.offset} ch: ${ch}, next state: ${nextState}, next class: ${nextClass}`);
+
+    if (nextState >= 0) {
+      // change the state
+      this.state = nextState;
+    } else {
+      // or perform on of the actions
+      switch (nextState){
+        case -3: // * + ?
+          //console.log(this.quantifiable);
+          if (!this.quantifiable) {
+            this.onError();
+          }
+          this.quantifiable = false;
+          this.state = OK;
+          break;
+        case -2: // atom
+          this.quantifiable = true;
+          this.state = OK;
+          break;
+        default:
+          this.onError();
+          break;
+      }
+    }
+
+    this.offset++;
+  }
+
+  public finalCheck(): void {
+    // the finalCheck function shoud be called after all of the characters
+    // have been processed, but only if every call to check returned
+    // without throwing an error. This method throws an error if the
+    // regular expression was not accepted; in other words, the final check failed.
+
+    if (this.state !== OK) {
+      this.onError();
+    }
+
+    this.pop(Mode.DONE);
+  }
+  private push(mode: Mode): void {
+    // push a mode onto the stack.
+    this.stack.push(mode);
+  }
+  private pop(mode: Mode): void {
+    // pop the stack, ensuring the current mode matches the expectation
+    // throw and error is the modes mismatch.
+    if (this.stack.pop() !== mode) {
+      this.onError();
+    }
+  }
+  private onError(): void {
+    throw new Error(`invalid regular expression at character offset ${this.offset}`);
   }
 }
 
-export const Checker = new IRegexpChecker();
+class IRegexp {
+  public check(expression: string): boolean{
+    try{
+      const checker = new IRegexpChecker();
+      [...expression]
+        .map(s => s.codePointAt(0)!)
+        .map(ch => checker.check(ch))
+      ;
+      checker.finalCheck();
+      return true;
+    }
+    catch (e) {
+      return false;
+    }
+  }
+}
+
+export const Checker = new IRegexp();
 export default Checker;
--- test/iregexp.spec.ts
+++ test/iregexp.spec.ts
@@ -1,7 +1,147 @@
-import { check } from "../src";
+import { check } from '../src';
 
-describe("iregexp", () => {
-  it("should parse", () => {
-    expect(check(".")).toEqual(true);
+describe('iregexp atoms', () => {
+  it('%x00-27', () => {
+    pass('\u0000');
+    pass('\u0001');
+    pass('\u0002');
+    pass('\u0003');
+    pass('\u0004');
+    pass('\u0005');
+    pass('\u0006');
+    pass('\u0007');
+    pass('\u0008');
+    pass('\u0009');
+    pass('\u000a');
+    pass('\u000b');
+    pass('\u000c');
+    pass('\u000d');
+    pass('\u000e');
+    pass('\u000f');
+    pass('\u0010');
+    pass('\u0011');
+    pass('\u0012');
+    pass('\u0013');
+    pass('\u0014');
+    pass('\u0015');
+    pass('\u0016');
+    pass('\u0017');
+    pass('\u0018');
+    pass('\u0019');
+    pass('\u001a');
+    pass('\u001b');
+    pass('\u001c');
+    pass('\u001d');
+    pass('\u001e');
+    pass('\u001f');
+    pass('\u0020');
+    pass('\u0021');
+    pass('\u0022');
+    pass('\u0023');
+    pass('\u0024');
+    pass('\u0025');
+    pass('\u0026');
+    pass('\u0027');
+  });
+  it('%x40-5A', () => {
+    pass('\u0040');
+    pass('\u0041');
+    pass('\u0042');
+    pass('\u0043');
+    pass('\u0044');
+    pass('\u0045');
+    pass('\u0046');
+    pass('\u0047');
+    pass('\u0048');
+    pass('\u0049');
+    pass('\u004a');
+    pass('\u004b');
+    pass('\u004c');
+    pass('\u004d');
+    pass('\u004e');
+    pass('\u004f');
+    pass('\u0050');
+    pass('\u0051');
+    pass('\u0052');
+    pass('\u0053');
+    pass('\u0054');
+    pass('\u0055');
+    pass('\u0056');
+    pass('\u0057');
+    pass('\u0058');
+    pass('\u0059');
+    pass('\u005a');
+  });
+  it('%x5E-7A', () => {
+    pass('\u005e');
+    pass('\u005f');
+    pass('\u0060');
+    pass('\u0061');
+    pass('\u0062');
+    pass('\u0063');
+    pass('\u0064');
+    pass('\u0065');
+    pass('\u0066');
+    pass('\u0067');
+    pass('\u0068');
+    pass('\u0069');
+    pass('\u006a');
+    pass('\u006b');
+    pass('\u006c');
+    pass('\u006d');
+    pass('\u006e');
+    pass('\u006f');
+    pass('\u0070');
+    pass('\u0071');
+    pass('\u0072');
+    pass('\u0073');
+    pass('\u0074');
+    pass('\u0075');
+    pass('\u0076');
+    pass('\u0077');
+    pass('\u0078');
+    pass('\u0079');
+    pass('\u007a');
+  });
+  it('%x7E-10FFF', () => {
+    pass('\u007e');
+    pass('\u007f');
+    pass('\u0080');
+    pass('\u0081');
+    // ...
   });
 });
+
+describe('iregexp quantifiers', () => {
+  it('quantifier', () => {
+    pass('a*');
+    pass('a+');
+    pass('a?');
+  });
+  //it('quantity', () => {
+  //  pass('a{42}');
+  //  pass('a{42,43}');
+  //});
+  it('should fail on invalid quantifiers', () => {
+    fail("*");
+    fail("+");
+    fail("?");
+  });
+});
+
+function pass(expression: string): void {
+  expect(check(expression)).toEqual(true);
+}
+function fail(expression: string): void {
+	let succeeded = false;
+	try{
+		succeeded = check(expression);
+	}
+	catch(e){
+		expect(e).toBeInstanceOf(Error);
+	}
+
+	if (succeeded){
+		throw new Error(`the expression ${expression} was incorrectly considered a valid regular expression.`);
+	}
+}
